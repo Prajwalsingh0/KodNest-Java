03100c276f78c4de35121e3dec429ef089d0d2cd
Prajwalsingh0 <prateeksingh8871055924@gmail.com>
2026-08-11 14:48:22 +0530

    Class & obj

---
+++ track/M03/ST01/FirstInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FirstInput {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter byte value : ");
+		byte a = scan.nextByte();
+		System.out.println(a);
+		System.out.println("Enter integer value : ");
+		int b = scan.nextInt();
+		System.out.println(b);
+	}
+}
