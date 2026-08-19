bf463fa954ab6ef5bad0ca30faf0c2f513817def
Prajwalsingh0 <prateeksingh8871055924@gmail.com>
2026-08-19 17:48:26 +0530

    Matrix

---
+++ track/M02/T01/ST01/Array/TwoDarray.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class TwoDarray {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a[][] = new int[3][5];
+
+        System.out.println("Enter Array Elements are :");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            for (int j = 0; j <= a[i].length - 1; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+
+        System.out.println("Array Elements are :");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            for (int j = 0; j <= a[i].length - 1; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
